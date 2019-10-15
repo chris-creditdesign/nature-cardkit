a45b77e5abd74f9dc90398c41f6fda4b5a7a659e
Chris Ryan <c.ryan@c02yx3uvlvdr.springernature.com>
2019-10-15 17:16:43 +0100

    Adds nature resdesign styles

--- src/configuration/layouts/main.js
+++ src/configuration/layouts/main.js
@@ -1,10 +1,3 @@
-const instagramCard = {
-	width: 1080,
-	height: 1080,
-	marginTop: 30,
-	marginSide: 30,
-}
-
 const twitterCard = {
 	width: 1066,
 	height: 600,
@@ -17,27 +10,12 @@ const circleLogo = {
 	height: 90,
 }
 
-const natureLogo = {
-	width: 180,
-	height: 35,
-}
-
-const futuresLogo = {
-	width: 150,
-	height: 145,
-}
-
 const twitterCreditLeft = {
 	card: twitterCard,
 	layers: {
 		headline: {
-			x: twitterCard.width / 2,
-			y: twitterCard.height * 0.25,
-		},
-		url: {
-			textAnchor: "end",
-			x: twitterCard.width - twitterCard.marginSide,
-			y: twitterCard.height - twitterCard.marginTop - 25,
+			x: 50,
+			y: 80,
 		},
 		credit: {
 			textAnchor: "start",
@@ -48,63 +26,10 @@ const twitterCreditLeft = {
 			transform: `translate(${twitterCard.width -
 				twitterCard.marginSide -
 				circleLogo.width}, ${twitterCard.marginTop})`,
-		},
-		natureLogo: {
-			transform: `translate(${
-				twitterCard.marginSide
-			}, ${twitterCard.height -
-				twitterCard.marginTop -
-				natureLogo.height})`,
-		},
-		futuresLogo: {
-			transform: `translate(${
-				twitterCard.marginSide
-			}, ${twitterCard.height -
-				twitterCard.marginTop -
-				futuresLogo.height})`,
-		},
+		}
 	},
 }
 
-const instagramCreditLeft = {
-	card: instagramCard,
-	layers: {
-		headline: {
-			x: instagramCard.width / 2,
-			y: instagramCard.height * 0.4,
-		},
-		url: {
-			textAnchor: "end",
-			x: instagramCard.width - instagramCard.marginSide,
-			y: instagramCard.height - instagramCard.marginTop - 20,
-		},
-		credit: {
-			textAnchor: "start",
-			x: instagramCard.marginSide,
-			y: instagramCard.height - instagramCard.marginTop - 15,
-		},
-		circleLogo: {
-			transform: `translate(${twitterCard.width -
-				twitterCard.marginSide -
-				circleLogo.width}, ${instagramCard.marginTop})`,
-		},
-		natureLogo: {
-			transform: `translate(${
-				instagramCard.marginSide
-			}, ${instagramCard.height -
-				instagramCard.marginTop -
-				natureLogo.height})`,
-		},
-		futuresLogo: {
-			transform: `translate(${
-				instagramCard.marginSide
-			}, ${instagramCard.height -
-				instagramCard.marginTop -
-				futuresLogo.height})`,
-		},
-	},
-}
 module.exports = {
-	twitter: twitterCreditLeft,
-	instagram: instagramCreditLeft,
+	twitter: twitterCreditLeft
 }
